ca00dba835be24bd55d962e19e35b7634daed486
vikrant123 <vikrant123@gmail.com>
2022-02-07 18:03:51 +0530

    Latest Updated

--- src/test/java/Apis/EnableDisable_TranactionStatus.java
+++ src/test/java/Apis/EnableDisable_TranactionStatus.java
@@ -23,9 +23,10 @@ public class EnableDisable_TranactionStatus
 	String res=	given().headers(Utilities.Headers()).
 	when().
 	get("/card-management/enablementcardservice/SPP000000539/CTLS/DOM").
-	then().assertThat().extract().response().toString();
-	
-	
+	then().assertThat().extract().response().asString();
+	System.out.println("--------Response--------");
+	System.out.println(res);
+	System.out.println("----------------");
 
 	}
 	
